e785f4c5001ceaa1b4af134f98c0280a89669dc0
halosatrio <63773815+halosatrio@users.noreply.github.com>
2021-03-22 13:34:00 +0700

    test fix

--- src/services/getHero.js
+++ src/services/getHero.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-import ids from "./ids";
+const key = process.env.REACT_APP_API_KEY;
 
 export async function getHeroLawan(id) {
   try {
     const response = await axios(
-      `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${ids[id]}.json`,
+      `https://www.superheroapi.com/api.php/${key}/${id}`,
       {
         method: "GET",
       }
     );
     return {
       name: response.data.name,
-      image: response.data.images.md,
+      image: response.data.image.url,
       intelligence: response.data.powerstats.intelligence,
       strength: response.data.powerstats.strength,
       speed: response.data.powerstats.speed,
@@ -28,14 +28,14 @@ export async function getHeroLawan(id) {
 export async function getHeroUser(id) {
   try {
     const response = await axios(
-      `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${ids[id]}.json`,
+      `https://www.superheroapi.com/api.php/${key}/${id}`,
       {
         method: "GET",
       }
     );
     return {
       name: response.data.name,
-      image: response.data.images.md,
+      image: response.data.image.url,
       intelligence: response.data.powerstats.intelligence,
       strength: response.data.powerstats.strength,
       speed: response.data.powerstats.speed,
